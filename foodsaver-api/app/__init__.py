470873b4a568c6c25d1839964428485bd29789f0
karima2022 <k.sebbah@it-students.fr>
2025-01-28 14:11:01 +0100

    fix config variable env pour bdd

--- foodsaver-api/app/__init__.py
+++ foodsaver-api/app/__init__.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 db = SQLAlchemy()
 cors = CORS()
 
-# fonction pour créer l'application Flask
+# Fonction pour créer l'application Flask
 def create_app():
     # Charger le fichier .env
     load_dotenv()
@@ -24,38 +24,38 @@ def create_app():
     MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
 
     ACCESS_EXPIRES = timedelta(hours=1)
+
     # Créer une instance de l'application Flask
     app = Flask(__name__)
 
-    import os
-
+    # Configurer SQLAlchemy
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    # Configurer JWT
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
 
+    # Initialiser les extensions
     db.init_app(app)
     migrate = Migrate(app, db)
     jwt = JWTManager(app)
     cors.init_app(app, resources={r"/*": {"origins": "*"}})
 
-
     # Swagger configuration
     SWAGGER_URL = "/swagger"  # URL for accessing Swagger UI
     API_URL = "/static/swagger.yaml"  # Path to your Swagger spec
+    swagger_ui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL)
+    app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
+    # Importer les modèles nécessaires
     from .models import User, TokenBlocklist
 
-    # Callback function to check if a JWT exists in the database blocklist
+    # Callback pour vérifier si un JWT est dans la blocklist
     @jwt.token_in_blocklist_loader
     def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
         jti = jwt_payload["jti"]
         token = db.session.query(TokenBlocklist.id).filter_by(jti=jti).scalar()
         return token is not None
-
-
-    swagger_ui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL)
-    app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
     # Importer les routes
     from .auth import auth

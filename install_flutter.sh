9660894bd91ef61caf5a07c48986f724cd1bf4ca
karima2022 <k.sebbah@it-students.fr>
2025-01-28 14:55:26 +0100

    fix script installation flutter

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Télécharge et installe Flutter
 FLUTTER_VERSION="3.10.6"  # Remplacez par la version Flutter souhaitée
 FLUTTER_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
 
@@ -6,5 +8,11 @@ echo "Downloading Flutter..."
 curl -LO $FLUTTER_URL
 tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 
-export PATH="$PATH:$PWD/flutter/bin"
+# Ajoute Flutter au PATH
+export PATH="$PWD/flutter/bin:$PATH"
+
+# Vérifie l'installation de Flutter
 flutter doctor
+
+# Vérifie si Flutter est bien dans le PATH
+echo "Flutter installed at $(which flutter)"

1a0d2ff3ec44594b414cf11633ddf7605f6f9395
MartinaIT <m.polakova@it-students.fr>
2025-01-30 14:33:45 +0100

    Fix bug image a la page login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -86,7 +86,7 @@ class _LoginScreenState extends State<LoginScreen> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Image.asset(
-                  '${prefixImage}assets/assets/images/welcome.png',
+                  '${prefixImage}assets/images/welcome.png',
                   width: MediaQuery.of(context).size.width - 585,
                 ),
                 Padding(

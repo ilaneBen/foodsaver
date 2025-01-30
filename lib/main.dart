83a22b91e3aa8f4834f893c3580b5f39ec23bd0b
MartinaIT <m.polakova@it-students.fr>
2025-01-30 13:53:58 +0100

    Update de .gitignore et .env-example

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Scanner OpenFoodFacts',
+      title: 'Scanner FoodSaver',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
